f44f17afd8bdc47aefa5c51f7c87a52c9806e356
Damars <damarsepti627@gmail.com>
2022-09-01 21:21:31 +0700

    -

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getNews() {
   const result = document.getElementById("result");
-  let url = "https://www.antaranews.com/rss/top-news";
+  let url = "https://lapi.kumparan.com/v2.0/rss/";
   fetch(url)
     .then((response) => response.text())
     .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
@@ -10,4 +10,13 @@ function getNews() {
     });
 }
 
-getNews();
+function getGempa() {
+  const url = "https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json";
+  fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+}

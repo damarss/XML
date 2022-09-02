f912d3ba459f5d2cb9f909ef9d8718ddc0e49ec3
Damar <damarsepti627@gmail.com>
2022-09-02 21:02:17 +0700

    fix function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-async function getNews() {
-  // const result = document.getElementById("result");
-  let url = "https://lapi.kumparan.com/v2.0/rss/";
-  return fetch(url, {
-    method: "GET",
-  })
+async function getNews(url) {
+  return fetch(url)
     .then((response) => response.text())
     .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
     .then((data) => {
@@ -11,22 +7,12 @@ async function getNews() {
     });
 }
 
-function getGempa() {
-  const url = "https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json";
-  fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE, CONNECT',
-      'Access-Control-Allow-Credentials': 'false',
-
-    }
-  })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
+async function getGempa(url) {
+  return fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
 }
-getNews();

51a61ed4b2596fa0eeac53c8ee8fd67adf8ace3c
Damar <damarsepti627@gmail.com>
2022-09-02 19:40:17 +0700

    return value

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-function getNews() {
+async function getNews() {
   // const result = document.getElementById("result");
   let url = "https://lapi.kumparan.com/v2.0/rss/";
-  fetch(url, {
+  return fetch(url, {
     method: "GET",
   })
     .then((response) => response.text())
     .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
     .then((data) => {
-      console.log(data)
-      console.log(data.getElementsByTagName("rss")[0]);
+      return data.getElementsByTagName("rss")[0];
     });
 }
 

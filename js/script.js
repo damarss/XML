51f70ffb76b47c7ca28095e642163cc64ea9a632
Damar <damarsepti627@gmail.com>
2022-09-03 12:23:39 +0700

    dom image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 async function getNews(url) {
   return fetch(url)
     .then((response) => response.text())
-    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((str) => new DOMParser().parseFromString(str, "text/xml"))
     .then((data) => {
-      return data.getElementsByTagName("rss")[0];
+      return data.querySelector("channel");
     });
 }
 
@@ -12,7 +12,28 @@ async function getGempa(url) {
     .then((response) => {
       return response.json();
     })
-    .then((data) => {
-      return data;
-    })
+}
+
+async function loadNews() {
+  const url = [
+    "https://coconuts.co/jakarta/feed/",
+    "https://lapi.kumparan.com/v2.0/rss/",
+    "https://www.cnnindonesia.com/ekonomi/rss"
+  ];
+  let news = [];
+  let source = "";
+  for (let i = 0; i < url.length; i++) {
+    news[i] = await getNews(url[i]);
+    console.log(news[i]);
+    source = news[i].querySelector("image").querySelector("url").textContent;
+    // document.write(`<img src='${source}' width='50' />`);
+    document.getElementById(`news-${i+1}`).innerHTML = `<img src='${source}' width='50' />`;
+  }
+
+}
+
+async function loadGempa() {
+  const url = "https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json";
+  const gempa = await getGempa(url);
+  console.log(gempa);
 }
